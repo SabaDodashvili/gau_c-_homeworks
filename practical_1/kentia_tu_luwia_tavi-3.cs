5dd65a86d4afa5676be9ca77a6c930caa0ec0d5a
SabaDodashvili <56235120+SabaDodashvili@users.noreply.github.com>
2022-03-07 18:04:05 +0400

    Add files via upload

---
+++ practical_1/kentia_tu_luwia_tavi-3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            if (num % 2 == 0 && num != 0)
+            {
+                Console.WriteLine("luwia");
+            }else if(num % 2 == 1)
+            {
+                Console.WriteLine("kentia");
+            }
+            else
+            {
+                Console.WriteLine("ricxvi 0 tolia");
+            }
+        }
+    }
+}
