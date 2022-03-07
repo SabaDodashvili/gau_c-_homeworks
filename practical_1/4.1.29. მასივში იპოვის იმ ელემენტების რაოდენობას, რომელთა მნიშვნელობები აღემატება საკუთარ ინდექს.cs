b1ecbb97b1712de873c5961dee616021b7daf479
SabaDodashvili <56235120+SabaDodashvili@users.noreply.github.com>
2022-03-07 18:31:57 +0400

    Add files via upload

---
+++ practical_1/4.1.29. მასივში იპოვის იმ ელემენტების რაოდენობას, რომელთა მნიშვნელობები აღემატება საკუთარ ინდექს.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = {5,6,3,4,112,4,9,190,10,0,-2,-5,23 };
+            int minValue = numbersArr[0];
+            int count = 0;
+
+            for (int i = 0; i < numbersArr.Length; i++)
+            {
+                if (numbersArr[i] > i)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ practical_1/4.1.4. იპოვის მასივის მინიმალურ ელემენტს.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = {5,6,3,4,112,4,9,190,10,0,-2,-5,23 };
+            int minValue = numbersArr[0];
+
+            for (int i = 1; i < numbersArr.Length; i++)
+            {
+                if (minValue > numbersArr[i])
+                {
+                    minValue = numbersArr[i];
+                }
+            }
+
+            Console.WriteLine(minValue);
+        }
+    }
+}
