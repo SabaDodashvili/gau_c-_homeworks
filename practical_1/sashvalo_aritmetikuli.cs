2689dc84ee066d3b31a921f69840a7ad3b0896c6
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-03-07 17:30:11 +0400

    create repository

---
+++ practical_1/sashvalo_aritmetikuli.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+            sum += numbers[i];
+            }
+
+            int arithmeticMean = sum / numbers.Length;
+
+            Console.WriteLine(arithmeticMean);
+        }
+    }
+}
